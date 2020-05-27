35f5438f2f722d0105e5b508540d309a74968cfa
Jose-cod7 <josegarriaga7@gmail.com>
2020-05-27 21:56:12 +0200

    tdd-practice

--- I.pass-tests/02-get-word-lengths/get-word-lengths.js
+++ I.pass-tests/02-get-word-lengths/get-word-lengths.js
@@ -1,3 +1,8 @@
-var getWordLengths = function(someWords) {};
+var getWordLengths = function(someWords) {
+    var someWords = ["sun", "potato", "roundabout", "pizza"];
+    return someWords.map((word) => word.length);
+};
 
 module.exports = getWordLengths;
+
+console.log(getWordLengths());
