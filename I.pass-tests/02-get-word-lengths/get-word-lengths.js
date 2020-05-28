5f26f0c972ec2fa2a5e26e1fa2614b3241eff52d
Jose-cod7 <josegarriaga7@gmail.com>
2020-05-28 12:23:01 +0200

    get-word-done

--- I.pass-tests/02-get-word-lengths/get-word-lengths.js
+++ I.pass-tests/02-get-word-lengths/get-word-lengths.js
@@ -1,8 +1,5 @@
 var getWordLengths = function(someWords) {
-    var someWords = ["sun", "potato", "roundabout", "pizza"];
     return someWords.map((word) => word.length);
 };
 
 module.exports = getWordLengths;
-
-console.log(getWordLengths());
