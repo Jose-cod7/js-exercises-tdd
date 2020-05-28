562486c48b01971b630578ae224099f04cc3bbf1
Jose-cod7 <josegarriaga7@gmail.com>
2020-05-28 15:43:55 +0200

    average

--- I.pass-tests/08-get-average/get-average.js
+++ I.pass-tests/08-get-average/get-average.js
@@ -1,3 +1,17 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
+
+function average(numbers) {
+    numbers.splice(1, 1);
+    numbers.splice(3, 2);
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
+    }
+    let ave = total / numbers.length;
+    console.log(ave);
+    return ave;
+}
+
+module.exports = average;
