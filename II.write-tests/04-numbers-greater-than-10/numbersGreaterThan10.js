e627044bbfc327d0069331fb945c338a96dee3ca
Jose-cod7 <josegarriaga7@gmail.com>
2020-05-30 17:53:44 +0200

    number-greater

--- II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.js
+++ II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.js
@@ -1,5 +1,9 @@
-module.exports = function(array) {
-  return array.filter(number => {
-    return number > 10;
-  });
-};
+function largerThanTen(array) {
+    return array.filter((number) => {
+        let boolean = number > 10;
+        console.log(boolean);
+        return number > 10;
+    });
+}
+
+module.exports = largerThanTen;
