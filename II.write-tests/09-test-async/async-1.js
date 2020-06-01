444b0ebb2c01478d99a3c3755abcb5d901a32d98
Jose-cod7 <josegarriaga7@gmail.com>
2020-06-01 18:31:26 +0200

    async-1

--- II.write-tests/09-test-async/async-1.js
+++ II.write-tests/09-test-async/async-1.js
@@ -1,12 +1,14 @@
 const fetch = require("node-fetch");
 
 const getRepos = function(repoName) {
-  return fetch(repoName)
-    .then(data => data.json())
+    return fetch(repoName)
+        .then((data) => data.json())
+
     .then(function(response) {
-      return response.map(function(rep) {
-        return rep.name;
-      });
+        return response.map(function(rep) {
+            console.log(rep.name);
+            return rep.name;
+        });
     });
 };
 
