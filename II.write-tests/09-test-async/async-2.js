c434f216ac3e193247b86b784d472ffbc4108857
Jose-cod7 <josegarriaga7@gmail.com>
2020-06-02 18:34:22 +0200

    asyn-mock

--- II.write-tests/09-test-async/async-2.js
+++ II.write-tests/09-test-async/async-2.js
@@ -1,11 +1,15 @@
 const fetcher = require("./fetcher");
 
 const getRepos = function(repoName) {
-  return fetcher(repoName).then(function(response) {
-    return response.map(function(rep) {
-      return rep.name;
+    return fetcher(repoName).then(function(response) {
+        return response.map(function(rep) {
+            //console.log(rep.name);
+            return rep.name;
+        });
     });
-  });
 };
 
 module.exports = getRepos;
+
+// line 3 (test.js file) exclude line 1 of js.file async-2. excluye el file fetcher.js. solo testea la función.
+// line 7 ((test.js file) exclude the call of the fetcher in line 4 of the file async 2 and replace with the promese response give back the mockResolvedValue.
--- II.write-tests/09-test-async/async-2.test.js
+++ II.write-tests/09-test-async/async-2.test.js
@@ -3,15 +3,18 @@ const fetcher = require("./fetcher");
 jest.mock("./fetcher");
 
 test("gets a list of repositories names (with mock)", function() {
-  // arrange
-  fetcher.mockResolvedValue([{ name: "js-exercises" }]);
-  var url = "https://api.github.com/users/kabaros/repos";
+    // arrange
+    fetcher.mockResolvedValue([{ name: "js-exercises" }]);
+    var url = "https://api.github.com/users/kabaros/repos";
 
-  // act
-  return getRepos(url).then(function(result) {
-    // assert
-    expect(result).toContain("js-exercises");
-  });
+    // act
+    return getRepos(url).then(function(result) {
+        // assert
+        console.log(result);
+        expect(result).toContain("js-exercises");
+        expect(fetcher).toHaveBeenCalledTimes(1);
+        expect(fetcher).toHaveBeenCalledWith(url);
+    });
 });
 
 test("a more deterministic test", function() {});
